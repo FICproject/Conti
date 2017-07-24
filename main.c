d55de67506cd687c6b311a8c1699d4f4b6ed48bc
FICproject <dvd_tudor@yahoo.com>
2017-07-24 09:05:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
-#include <stdio.h> #include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h>
 
 #define PROB 2
 
-#define PROB1 1 #define PROB2 2 #define PROB3 3 #define PROB4 4 #define PROB5 5 #define PROB6 6
+#define PROB1 1 
+#define PROB2 2 
+#define PROB3 3 
+#define PROB4 4 
+#define PROB5 5 
+#define PROB6 6
 
 void conv(unsigned long var);
 void inv(unsigned long param);
@@ -18,19 +24,25 @@ int main(){
 return 0;
 }
 
-#if(PROB == PROB1) void conv(unsigned long var) { int reminder, v[sizeof(var) * 8], nr_1 = 0, nr_0 = 0, k = 0; while (var != 0) { reminder = var % 2; var = var / 2;
+#if(PROB == PROB1) 
+void conv(unsigned long var) 
+{ 
+        int reminder, v[sizeof(var) * 8], nr_1 = 0, nr_0 = 0, k = 0;
+        while (var != 0) 
+        {
+                reminder = var % 2; var = var / 2;
 
-        if (reminder)
-        {
-            nr_1++;
-        }
-        else
-        {
-            nr_0++;
-        }
-        v[k] = reminder;
-        k++;
-    }
+                if (reminder)
+                {
+                 nr_1++;
+                }
+                else
+                {
+                 nr_0++;
+                }
+                v[k] = reminder;
+                k++;
+         }
     for (int i = k - 1; i >= 0; i--)
     {
         printf("%d ", v[i]);
